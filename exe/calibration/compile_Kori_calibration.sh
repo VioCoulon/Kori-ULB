146b73e496a722d3236873d1391a5ebc2003347c
Daniel <daniel.moreno.parada@ulb.be>
2026-08-11 17:36:57 +0200

    Save changes to fork to Icemodel2.

---
+++ exe/calibration/compile_Kori_calibration.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# Bash file to compile Kori-ULB and copy it to CECI cluster.
+# Execute it with: bash compile_Kori.sh
+path_kori=/home/daniel/models/Kori-ULB
+path_kori_subroutines=$path_kori/subroutines
+
+
+
+# LOCAL PATHS.
+path_m=$path_kori/calibration
+path_exe=$path_kori/exe/calibration      # CalvingMIP.
+
+exp="Dutrieux2009"
+
+# Enter path with matlab scripts to be compiled.
+cd $path_exe
+
+# Compiling options: individual_file, ensemble.
+option="individual_file"  
+
+
+##################################################################################
+# OPTION 1.
+if [ "$option" = "individual_file" ]; then
+
+echo "Compiling individual file..."
+
+# Compilation for single files.
+file_name=KoriCalibration.m
+exe_name=KoriCalibration_$exp
+
+echo "Compiling    : $file_name"
+echo "Exe_name     : $exe_name"
+#echo "path_cluster : $path_cluster"
+
+# Copy matlab file for compilation.
+cp $path_m/$file_name .
+
+mcc -m $file_name -a $path_kori/KoriModel.m -a $path_kori_subroutines -o $exe_name
+#ssh $REMOTE_HOST "mkdir -p $path_cluster"
+#rsync -avz --progress "$exe_name" "$REMOTE_HOST:$path_cluster"
+
+##################################################################################
+
+
+
+
+##################################################################################
+# OPTION 2.
+# Loop over each file in the directory.
+elif [ "$option" = "ensemble" ]; then
+
+echo "Compiling ensemble..."
+
+file=RunASE_ceci.m
+exe_name=RunASE_ceci
+
+# Compile Kori only once.
+echo "Path_par     : $path_param"
+echo "Path_exe     : $path_exe"
+echo "Path_cluster : $path_cluster"
+echo "Compiling    : $file"
+
+# RECOMPILE IN CASE OF CHANGES IN THE CODE!
+mcc -m "$file" -a "$path_kori/KoriModel.m" -a "$path_kori_subroutines" -o "$exe_name"
+rsync -avz --progress "$exe_name" "$REMOTE_HOST:$path_exe_cluster/"
+
+
+# Copy folder with param files.
+ssh $REMOTE_HOST "mkdir -p $path_cluster"
+rsync -avz --progress "$path_param" "$REMOTE_HOST:$path_cluster/"
+
+
+echo "Copying executable to all directories in a single SSH connection"
+#ssh $REMOTE_HOST "mkdir -p $path_cluster"
+
+# Copy via ssh only once and then copy within the cluster.
+# File(s) to copy (you can use wildcards like *.sh)
+# SSH command to copy files to all subdirectories
+ssh $REMOTE_HOST << EOF
+for dir in "$path_cluster/$exp"/*/; do
+    if [ -d "\$dir" ]; then
+        echo "Exp  : \$dir"
+        cp "$path_exe_cluster/$exe_name" "\$dir"
+    fi
+done
+EOF
+
+echo "All files copied!"
+
+##################################################################################
+
+fi
